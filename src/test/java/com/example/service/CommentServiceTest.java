e51a1e21857c5de545ebcd1b8478b6d95b001515
GorbunovIvan <gorbunovvivan@gmail.com>
2023-07-16 15:05:41 +0300

    tests for services are added

---
+++ src/test/java/com/example/service/CommentServiceTest.java
@@ -0,0 +1,190 @@
+package com.example.service;
+
+import com.example.exception.EntityNotFoundException;
+import com.example.model.Comment;
+import com.example.model.Post;
+import com.example.model.User;
+import com.example.repository.CommentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+@SpringBootTest
+class CommentServiceTest {
+
+    @Autowired
+    private CommentService commentService;
+
+    @MockBean
+    private CommentRepository commentRepository;
+
+    private List<Comment> comments;
+    private List<Post> posts;
+    private List<User> users;
+
+    private Comment newComment;
+
+    @BeforeEach
+    void setUp() {
+
+        users = List.of(
+                User.builder().id(1L).name("1 user test").createdAt(LocalDateTime.now()).build(),
+                User.builder().id(2L).name("2 user test").createdAt(LocalDateTime.now()).build()
+        );
+
+        posts = List.of(
+                Post.builder().id(1L).title("1 title post").content("1 content post").createdAt(LocalDateTime.now()).updatedAt(LocalDateTime.now()).user(users.get(1)).build(),
+                Post.builder().id(2L).title("2 title post").content("2 content post").createdAt(LocalDateTime.now()).updatedAt(LocalDateTime.now()).user(users.get(0)).build()
+        );
+
+        comments = List.of(
+                Comment.builder().id(1L).content("1 content comment").createdAt(LocalDateTime.now()).post(posts.get(0)).user(users.get(1)).build(),
+                Comment.builder().id(2L).content("2 content comment").createdAt(LocalDateTime.now()).post(posts.get(1)).user(users.get(0)).build(),
+                Comment.builder().id(3L).content("3 content comment").createdAt(LocalDateTime.now()).post(posts.get(0)).user(users.get(0)).build()
+        );
+
+        for (var user : users) {
+            user.setPosts(posts.stream()
+                    .filter(p -> p.getUser().equals(user))
+                    .collect(Collectors.toSet()));
+        }
+
+        for (var user : users) {
+            user.setComments(comments.stream()
+                    .filter(c -> c.getUser().equals(user))
+                    .collect(Collectors.toSet()));
+        }
+
+        for (var post : posts) {
+            post.setComments(comments.stream()
+                    .filter(c -> c.getPost().equals(post))
+                    .collect(Collectors.toSet()));
+        }
+
+        newComment = Comment.builder().id(4L).content("new content comment").createdAt(LocalDateTime.now()).post(posts.get(0)).user(users.get(0)).build();
+
+        when(commentRepository.findAll()).thenReturn(comments);
+        when(commentRepository.findAllByUserIdOrderByCreatedAt(-1L)).thenReturn(Collections.emptyList());
+        when(commentRepository.findAllByUserIdOrderByCreatedAt(-1L)).thenReturn(Collections.emptyList());
+        when(commentRepository.findById(-1L)).thenReturn(Optional.empty());
+        when(commentRepository.existsById(-1L)).thenReturn(false);
+        when(commentRepository.save(newComment)).thenReturn(newComment);
+        doNothing().when(commentRepository).deleteById(anyLong());
+
+        for (var comment : comments) {
+            when(commentRepository.findById(comment.getId())).thenReturn(Optional.of(comment));
+            when(commentRepository.existsById(comment.getId())).thenReturn(true);
+            when(commentRepository.save(comment)).thenReturn(comment);
+        }
+
+        for (var user : users) {
+            when(commentRepository.findAllByUserIdOrderByCreatedAt(user.getId())).thenReturn(new ArrayList<>(user.getComments()));
+        }
+
+        for (var post : posts) {
+            when(commentRepository.findAllByPostIdOrderByCreatedAt(post.getId())).thenReturn(new ArrayList<>(post.getComments()));
+        }
+    }
+
+    @Test
+    void testGetAll() {
+        assertEquals(comments, commentService.getAll());
+        verify(commentRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getAllByUserId() {
+
+        for (var user : users) {
+
+            var commentsExpected = new ArrayList<>(user.getComments());
+            var comments = commentService.getAllByUserId(user.getId());
+
+            commentsExpected.sort(Comparator.comparing(Comment::getId));
+            comments.sort(Comparator.comparing(Comment::getId));
+
+            assertEquals(commentsExpected, comments);
+            verify(commentRepository, times(1)).findAllByUserIdOrderByCreatedAt(user.getId());
+        }
+
+        assertTrue(commentService.getAllByUserId(-1L).isEmpty());
+        verify(commentRepository, times(1)).findAllByUserIdOrderByCreatedAt(-1L);
+    }
+
+    @Test
+    void getAllByPostId() {
+
+        for (var post : posts) {
+
+            var commentsExpected = new ArrayList<>(post.getComments());
+            var comments = commentService.getAllByPostId(post.getId());
+
+            commentsExpected.sort(Comparator.comparing(Comment::getId));
+            comments.sort(Comparator.comparing(Comment::getId));
+
+            assertEquals(commentsExpected, comments);
+            verify(commentRepository, times(1)).findAllByPostIdOrderByCreatedAt(post.getId());
+        }
+
+        assertTrue(commentService.getAllByPostId(-1L).isEmpty());
+        verify(commentRepository, times(1)).findAllByPostIdOrderByCreatedAt(-1L);
+    }
+
+    @Test
+    void testGetById() {
+
+        for (var comment : comments) {
+            assertEquals(comment, commentService.getById(comment.getId()));
+            verify(commentRepository, times(1)).findById(comment.getId());
+        }
+
+        assertNull(commentService.getById(-1L));
+        verify(commentRepository, times(1)).findById(-1L);
+    }
+
+    @Test
+    void testCreate() {
+        assertEquals(newComment, commentService.create(newComment));
+        verify(commentRepository, times(1)).save(newComment);
+    }
+
+    @Test
+    void testUpdate() {
+
+        for (var comment : comments) {
+            comment.setContent(comment.getContent() + " updated");
+            assertEquals(comment, commentService.update(comment.getId(), comment));
+            verify(commentRepository, times(1)).existsById(comment.getId());
+            verify(commentRepository, times(1)).save(comment);
+        }
+
+        assertThrows(EntityNotFoundException.class, () -> commentService.update(-1L, newComment));
+        verify(commentRepository, times(1)).existsById(-1L);
+        verify(commentRepository, never()).save(newComment);
+    }
+
+    @Test
+    void testDeleteById() {
+
+        for (var comment : comments) {
+            commentService.deleteById(comment.getId());
+            verify(commentRepository, times(1)).existsById(comment.getId());
+            verify(commentRepository, times(1)).deleteById(comment.getId());
+        }
+
+        assertThrows(EntityNotFoundException.class, () -> commentService.deleteById(-1L));
+        verify(commentRepository, times(1)).existsById(-1L);
+        verify(commentRepository, never()).deleteById(-1L);
+    }
+}
---
+++ src/test/java/com/example/service/UserServiceTest.java
@@ -0,0 +1,136 @@
+package com.example.service;
+
+import com.example.exception.EntityNotFoundException;
+import com.example.model.User;
+import com.example.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    private List<User> users;
+    private User newUser;
+
+    @BeforeEach
+    void setUp() {
+
+        users = List.of(
+                User.builder().id(1L).name("1 user test").createdAt(LocalDateTime.now()).build(),
+                User.builder().id(2L).name("2 user test").createdAt(LocalDateTime.now()).build(),
+                User.builder().id(3L).name("3 user test").createdAt(LocalDateTime.now()).build()
+        );
+
+        newUser = User.builder().id(4L).name("new user test").build();
+
+        when(userRepository.findAll()).thenReturn(users);
+        when(userRepository.findById(-1L)).thenReturn(Optional.empty());
+        when(userRepository.findByIdEagerly(-1L)).thenReturn(Optional.empty());
+        when(userRepository.findByIdWithSubscriptions(-1L)).thenReturn(Optional.empty());
+        when(userRepository.existsById(-1L)).thenReturn(false);
+        when(userRepository.save(newUser)).thenReturn(newUser);
+        doNothing().when(userRepository).deleteById(anyLong());
+
+        for (var user : users) {
+            when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+            when(userRepository.findByIdEagerly(user.getId())).thenReturn(Optional.of(user));
+            when(userRepository.findByIdWithSubscriptions(user.getId())).thenReturn(Optional.of(user));
+            when(userRepository.existsById(user.getId())).thenReturn(true);
+            when(userRepository.save(user)).thenReturn(user);
+        }
+    }
+
+    @Test
+    void testGetAll() {
+        assertEquals(users, userService.getAll());
+        verify(userRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testGetById() {
+
+        for (var user : users) {
+            assertEquals(user, userService.getById(user.getId()));
+            verify(userRepository, times(1)).findById(user.getId());
+        }
+
+        assertNull(userService.getById(-1L));
+        verify(userRepository, times(1)).findById(-1L);
+    }
+
+    @Test
+    void testGetByIdEagerly() {
+
+        for (var user : users) {
+            assertEquals(user, userService.getByIdEagerly(user.getId()));
+            verify(userRepository, times(1)).findByIdEagerly(user.getId());
+        }
+
+        assertNull(userService.getByIdEagerly(-1L));
+        verify(userRepository, times(1)).findByIdEagerly(-1L);
+    }
+
+    @Test
+    void testGetByIdWithSubscriptions() {
+
+        for (var user : users) {
+            assertEquals(user, userService.getByIdWithSubscriptions(user.getId()));
+            verify(userRepository, times(1)).findByIdWithSubscriptions(user.getId());
+        }
+
+        assertNull(userService.getByIdWithSubscriptions(-1L));
+        verify(userRepository, times(1)).findByIdWithSubscriptions(-1L);
+    }
+
+    @Test
+    void testCreate() {
+        assertEquals(newUser, userService.create(newUser));
+        verify(userRepository, times(1)).save(newUser);
+    }
+
+    @Test
+    void testUpdate() {
+
+        for (var user : users) {
+            user.setName(user.getName() + " updated");
+            assertEquals(user, userService.update(user.getId(), user));
+            verify(userRepository, times(1)).existsById(user.getId());
+            verify(userRepository, times(1)).save(user);
+        }
+
+        assertThrows(EntityNotFoundException.class, () -> userService.update(-1L, newUser));
+        verify(userRepository, times(1)).existsById(-1L);
+        verify(userRepository, never()).save(newUser);
+    }
+
+    @Test
+    void testDeleteById() {
+
+        for (var user : users) {
+            userService.deleteById(user.getId());
+            verify(userRepository, times(1)).existsById(user.getId());
+            verify(userRepository, times(1)).deleteById(user.getId());
+        }
+
+        assertThrows(EntityNotFoundException.class, () -> userService.deleteById(-1L));
+        verify(userRepository, times(1)).existsById(-1L);
+        verify(userRepository, never()).deleteById(-1L);
+    }
+}
